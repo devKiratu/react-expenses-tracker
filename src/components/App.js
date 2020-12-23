907f213ae578434e920b82cbaaa69a980930ecab
devkiratu <dev.kiratu@gmail.com>
2020-12-23 10:04:29 +0300

    initialize and customize project setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { GlobalStyles } from "../styles/GlobalStyles";
+
+export default function App() {
+	return (
+		<div>
+			<GlobalStyles />
+			Hello React
+		</div>
+	);
+}
